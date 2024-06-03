696a0fce6c99810fbb5ff06b514a9b247f5bae6c
wael <78425538+wa20@users.noreply.github.com>
2024-06-03 15:09:10 +0100

    created inital postgres setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./db');
 
 const app = express();
 port = 5440
@@ -11,6 +12,17 @@ app.use(express.json());
 app.use(express.json()) //this is how we request data from the client side
 
 
+//Routes
+
+//create a todo
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(port, () => { 
     console.log(`Server is running on port ${port}`);
